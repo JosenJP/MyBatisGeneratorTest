78049ff3dc617e35f75f52119ce86ae25e0f5a78
Josen <jiang891203@126.com>
2017-12-03 22:18:23 +0800

    Enable the auto commit of the SqlSession

--- MyBatisGeneratorTest/Josen/src/main/java/com/Josen/MyBatisGeneratorTest/App.java
+++ MyBatisGeneratorTest/Josen/src/main/java/com/Josen/MyBatisGeneratorTest/App.java
@@ -2,6 +2,7 @@ package com.Josen.MyBatisGeneratorTest;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -29,11 +30,13 @@ public class App
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         
         
-        SqlSession session = sqlSessionFactory.openSession();
+        SqlSession session = sqlSessionFactory.openSession(true); //Make the session autocommite true. Default autocommit is false.
         try {
             
             StudentsDomainObjectMapper mapper = session.getMapper(StudentsDomainObjectMapper.class);
             
+            
+            //Query
             StudentsDomainObject student = mapper.selectByPrimaryKey("000000001");
             
             if (null != student)
@@ -41,12 +44,50 @@ public class App
                 System.out.println(String.format("The Age of %s is %d.", student.getName(), student.getAge()));
             }
             
+            //Insert
+            StudentsDomainObject student1 = new StudentsDomainObject();
+            student1.setID("000000003");
+            student1.setName("Miyuer");
+            student1.setAge(new Integer(28));
+            
+            mapper.insert(student1);
+            
+            List<StudentsDomainObject> students = mapper.selectAll();
+            System.out.println(String.format("Total %d students, include Miyuer", students.size()));
+            PrintAllStudents(students);
+            
+            //Update
+            student = mapper.selectByPrimaryKey("000000001");
+            student.setAge(30);
+            
+            mapper.updateByPrimaryKey(student);
+            System.out.println(String.format("Update the age of %s", "000000001"));
+            students = mapper.selectAll();
+            PrintAllStudents(students);
+            
+            //Delete
+            mapper.deleteByPrimaryKey("000000003");
+            students = mapper.selectAll();
+            System.out.println(String.format("Total %d students. Miyuer is leaving", students.size()));
+            PrintAllStudents(students);
         }
         catch (Exception ex)
         {
             System.out.println(ex.getMessage());
+        }
+        finally 
+        {
+            //session.commit(); //If the session is not autocommit, need to commit manually.
             session.close();
         }
 
+    }
+    
+    public static void PrintAllStudents(List<StudentsDomainObject> students)
+    {
+        for (StudentsDomainObject one : students)
+        {
+            System.out.println(String.format("Name:%s Age:%d", one.getName(), one.getAge()));
+        }
     }
 }
